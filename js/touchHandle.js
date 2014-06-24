0e33a56a5cc9b2a639d612de81a761ce5402ad5a
Jshauk <gavincrenshaw95@gmail.com>
2014-06-24 15:04:08 -0400

    Wrong script commited last time

--- js/touchHandle.js
+++ js/touchHandle.js
@@ -2,8 +2,6 @@
  * Created by gavin on 6/24/14.
  */
 $.event.special.tap = {
-    // Abort tap if touch lasts longer than half a second
-    timeThreshold: 500,
     setup: function() {
         var self = this,
             $self = $(self);
@@ -11,30 +9,17 @@ $.event.special.tap = {
         // Bind touch start
         $self.on('touchstart', function(startEvent) {
             // Save the target element of the start event
-            var target = startEvent.target,
-                timeout;
+            var target = startEvent.target;
 
-            function removeTapHandler() {
-                clearTimeout(timeout);
-                $self.off('touchend', tapHandler);
-            };
-
-            function tapHandler(endEvent) {
-                removeTapHandler();
-
+            // When a touch starts, bind a touch end handler exactly once,
+            $self.one('touchend', function(endEvent) {
                 // When the touch end event fires, check if the target of the
                 // touch end is the same as the target of the start, and if
                 // so, fire a click.
                 if (target == endEvent.target) {
                     $.event.simulate('tap', self, endEvent);
                 }
-            };
-
-            // Remove the tap handler if the timeout expires
-            timeout = setTimeout(removeTapHandler, $.event.special.tap.timeThreshold);
-
-            // When a touch starts, bind a touch end handler
-            $self.on('touchend', tapHandler);
+            });
         });
     }
 };
