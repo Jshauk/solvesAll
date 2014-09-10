50ac396bcbf5c6de258906c87804bce8bbd9590c
Jshauk <gavincrenshaw95@gmail.com>
2014-09-10 18:47:58 -0400

    Removed build directories

--- platforms/android/ant-gen/com/aestheticdevelopment/solvesAll/R.java
+++ platforms/android/ant-gen/com/aestheticdevelopment/solvesAll/R.java
@@ -13,6 +13,7 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
         public static final int screen=0x7f020001;
+        public static final int splash=0x7f020002;
     }
     public static final class integer {
         public static final int google_play_services_version=0x7f050000;
